eaf8ec29ed81f22ea7258a4228427f0536ed1cc0
Zak <112857232+coolp9722@users.noreply.github.com>
2024-04-04 10:38:03 +1100

    update
    update

--- massdrop.py
+++ massdrop.py
@@ -12,19 +12,24 @@ init()
 os.system("clear || cls")
 print(Fore.LIGHTMAGENTA_EX + "AIRDROP SPAMMER MADE BY ZAK")
 
+def loading_animation():
+    animation = "|/-\\"
+    start_time = time.time()
+    while time.time() - start_time < 3:
+        for char in animation:
+            print(Fore.LIGHTMAGENTA_EX + f" Loading {char}", end="\r")
+            time.sleep(0.1)
+
+loading_animation()
 def send_to_recipient(recipient, content_url):
     command = f"opendrop send -r {recipient} -f {content_url} --url"
     try:
         while True:
             subprocess.run(command, shell=True, check=True, stderr=subprocess.DEVNULL)
             print(Fore.LIGHTGREEN_EX + f"[+] sent to recipient {recipient} successfully.")
-            
-            # Send a new airdrop to the same recipient
             new_command = f"opendrop send -r {recipient} -f {content_url} --url"
             subprocess.run(new_command, shell=True, check=True, stderr=subprocess.DEVNULL)
             print(Fore.LIGHTYELLOW_EX + f"[+] New airdrop sent to recipient {recipient} successfully.")
-            
-            # Add a delay before sending the new airdrop to avoid spamming
             time.sleep(1)
             
     except Exception as e:
@@ -41,9 +46,11 @@ def send_to_all_recipients(recipients, content_url):
         thread.join()
 
 def main():
-    num_recipients = int(input(Fore.LIGHTMAGENTA_EX + "Enter the number of recipients: "))
+    num_recipients = int(input(Fore.LIGHTMAGENTA_EX + "Enter the number of recipients: ")) + 1
+    url = input(Fore.LIGHTMAGENTA_EX + "Enter the content url: ")
     recipients = list(range(num_recipients))
-    content_url = "https://www.youtube.com/watch?v=1P5yyeeYF9o"
+    content_url = url
+    print(Fore.LIGHTGREEN_EX + f"[+] Sending airdrop to {num_recipients} recipients with content url {content_url}")
     while True:
         send_to_all_recipients(recipients, content_url)
 
@@ -64,8 +71,11 @@ def find_Computers():
             print(line.decode().strip())
         process.wait()
         
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e}")
+    except subprocess.CalledProcessError:
+        print(Fore.LIGHTRED_EX + f"Error")
+    except KeyboardInterrupt:
+       os.system("clear")
+       print_code()
 
 
 def print_code():
